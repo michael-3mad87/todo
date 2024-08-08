16c833752fd1cbb52c1e99848548c80b83433fc1
michael-3mad87 <michael.3mad87@gmail.com>
2024-08-08 17:32:40 +0300

    finish home Screen ui and taps logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo/app_them.dart';
+import 'package:todo/home_page.dart';
+import 'package:todo/taps/tasks/task_tap.dart';
+import 'package:todo/taps/setting/setting_tap.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +14,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     
+      debugShowCheckedModeBanner: false,
+      routes: {
+        TaskPage.routName: (_) => TaskPage(),
+        SettingPage.routName: (_) => SettingPage(),
+        HomePage.routName:(_) => HomePage(),
+      },
+      initialRoute: HomePage.routName,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.light,
     );
   }
 }
-
--- lib/taps/setting/setting_tap.dart
+++ lib/taps/setting/setting_tap.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
+     static String routName = 'setting/';
 
   @override
   Widget build(BuildContext context) {
